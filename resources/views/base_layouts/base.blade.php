2990d09ba63c83222d99255215558c477c7d0a7d
Lowell256 <lowellvillegas256@gmail.com>
2018-07-05 16:38:00 +0800

    styles/login front end

---
+++ resources/views/base_layouts/base.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link href="{{ asset('fa/css/all.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/mdb.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+<body>
+<div class="container-fluid">
+    @yield('contents')
+</div>
+<script type="text/javascript" src="{{ asset('js/jquery331.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/popper.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/mdb.min.js') }}"></script>
+<script type="text/javascript">
+    @yield('script')
+</script>
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,31 @@
+@extends('base_layouts.base')
+
+@section('contents')
+</style>
+<div class="row">
+<div class="col-xs-12 col-md-4 offset-md-4 login-card">
+    <div class="card login-card">
+    <div class="card-header success-color white-text">
+            <i class="fas fa-user"></i> Login
+    </div>
+        <div class="card-body">
+            <form>
+                <div class="md-form">
+                    <i class="fas fa-user-circle prefix grey-text"></i>
+                    <input type="text" class="form-control">
+                    <label>Username</label>
+                </div>
+                <div class="md-form">
+                    <i class="fas fa-lock prefix grey-text"></i>
+                    <input type="password" class="form-control">
+                    <label>Password</label>
+                </div>
+                <button class="btn btn-outline-success waves-effect btn-sm" type="submit">Login</button>
+            </form>
+        </div>  
+        <div class="card-footer text-muted success-color white-text">
+        </div>
+    </div>
+</div>
+</div>
+@endsection
